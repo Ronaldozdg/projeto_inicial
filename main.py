76a7545dcf2cdbacdca31735f7820b76029c1558
jornada de Dados <ronaldoflorenciozdg@gmail.com>
2024-05-23 11:13:43 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("jornada de dados")
+
+# Variáveis
+idade = 50
+nome ='ronaldo'
+
+# Print
+print(idade)
+
+# Soma
+
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+Lista = [1,2,3,4,5]
+
+# Métodos
+Lista_invertida = Lista.reverse()
+
